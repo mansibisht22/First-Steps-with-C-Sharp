1079e1f3a7e21048aae9f55e4659e71e666e3479
Mansi Bisht <84663893+mansibisht22@users.noreply.github.com>
2021-08-31 14:47:19 +0530

    Add files via upload

---
+++ DataTypes.cs
@@ -0,0 +1,13 @@
+using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int myNum = 5;               // Integer (whole number)
+        double myDoubleNum = 5.99D;  // Floating point number
+        char myLetter = 'D';         // Character
+        bool myBool = true;          // Boolean
+        string myText = "Hello";     // String
+
+    }
+}
